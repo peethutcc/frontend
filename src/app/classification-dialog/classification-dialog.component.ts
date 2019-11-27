4f65c7fbf24ad7d241bda11a7f7ce82bd01cc836
bob <bob@gmail.com>
2019-11-28 00:06:55 +0700

    แก้ไขตาราง

--- src/app/classification-dialog/classification-dialog.component.ts
+++ src/app/classification-dialog/classification-dialog.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ParseapiService } from '../parseapi.service';
+
+import {FormControl, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 @Component({
   selector: 'app-classification-dialog',
   templateUrl: './classification-dialog.component.html',
@@ -10,10 +15,34 @@ export class ClassificationDialogComponent implements OnInit {
   constructor(public ps:ParseapiService) {
     ps.init();
    }
+   //ทำ AutoCompete------------------
+  myControl = new FormControl();
+  options = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+ //-----------------------
+
 ob;
   ngOnInit() {
     this.ob =this.ps.getid();
+
+    //--ทำ AutoCompete-------------------------------
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+    //---------------------------------------------
+
   }
+
+//--ทำ AutoCompete----------------------------------
+private _filter(value: string): string[] {
+  const filterValue = value.toLowerCase();
+
+  return this.options.filter(option => option.toLowerCase().includes(filterValue));
+}
+//----------------------------------------------
+
   classificationdata(files,ogManuscript,copy,status){
     this.ps.classificationData(files,ogManuscript,copy,status);
     
